2bf1c54aa3b11311c7b872e9195b751f54f28906
Ed Flanagan <ef@stripe.com>
2019-04-24 13:25:34 -0700

    gofmt
    Apply 'gofmt -w -s .'

--- hive_test.go
+++ hive_test.go
@@ -175,7 +175,7 @@ func TestDescription(t *testing.T) {
 
 	// We come from an insert
 	d := cursor.Description()
-	expected := [][]string{[]string{"col1", "INT_TYPE"}, []string{"col2", "STRING_TYPE"}}
+	expected := [][]string{{"col1", "INT_TYPE"}, {"col2", "STRING_TYPE"}}
 	if !reflect.DeepEqual(d, expected) {
 		t.Fatalf("Expected map: %+v, got: %+v", expected, d)
 	}
@@ -189,7 +189,7 @@ func TestDescription(t *testing.T) {
 	}
 
 	d = cursor.Description()
-	expected = [][]string{[]string{"pokes.a", "INT_TYPE"}, []string{"pokes.b", "STRING_TYPE"}}
+	expected = [][]string{{"pokes.a", "INT_TYPE"}, {"pokes.b", "STRING_TYPE"}}
 	if !reflect.DeepEqual(d, expected) {
 		t.Fatalf("Expected map: %+v, got: %+v", expected, d)
 	}
@@ -209,7 +209,7 @@ func TestDescription(t *testing.T) {
 		t.Fatal(cursor.Err)
 	}
 
-	expected = [][]string{[]string{"pokes.a", "INT_TYPE"}, []string{"pokes.b", "STRING_TYPE"}}
+	expected = [][]string{{"pokes.a", "INT_TYPE"}, {"pokes.b", "STRING_TYPE"}}
 	if !reflect.DeepEqual(d, expected) {
 		t.Fatalf("Expected map: %+v, got: %+v", expected, d)
 	}
@@ -233,7 +233,7 @@ func TestDescriptionAsync(t *testing.T) {
 	}
 
 	d := cursor.Description()
-	expected := [][]string{[]string{"pokes.a", "INT_TYPE"}, []string{"pokes.b", "STRING_TYPE"}}
+	expected := [][]string{{"pokes.a", "INT_TYPE"}, {"pokes.b", "STRING_TYPE"}}
 	if !reflect.DeepEqual(d, expected) {
 		t.Fatalf("Expected map: %+v, got: %+v", expected, d)
 	}
@@ -433,21 +433,21 @@ func TestRowMapAllTypes(t *testing.T) {
 	}
 	m := cursor.RowMap(context.Background())
 	expected := map[string]interface{}{
-		"all_types.smallint": int16(32767), 
-		"all_types.int": int32(2147483647), 
-		"all_types.float": float64(0.5), 
-		"all_types.double": float64(0.25), 
-		"all_types.string": "a string", 
-		"all_types.boolean": true, 
-		"all_types.struct": "{\"a\":1,\"b\":2}", 
-		"all_types.bigint": int64(9223372036854775807), 
-		"all_types.array": "[1,2]", 
-		"all_types.map": "{1:2,3:4}", 
-		"all_types.decimal": "0.1", 
-		"all_types.binary": []uint8{49, 50, 51}, 
-		"all_types.timestamp": "1970-01-01 00:00:00", 
-		"all_types.union": "{0:1}", 
-		"all_types.tinyint": int8(127), 
+		"all_types.smallint":  int16(32767),
+		"all_types.int":       int32(2147483647),
+		"all_types.float":     float64(0.5),
+		"all_types.double":    float64(0.25),
+		"all_types.string":    "a string",
+		"all_types.boolean":   true,
+		"all_types.struct":    "{\"a\":1,\"b\":2}",
+		"all_types.bigint":    int64(9223372036854775807),
+		"all_types.array":     "[1,2]",
+		"all_types.map":       "{1:2,3:4}",
+		"all_types.decimal":   "0.1",
+		"all_types.binary":    []uint8{49, 50, 51},
+		"all_types.timestamp": "1970-01-01 00:00:00",
+		"all_types.union":     "{0:1}",
+		"all_types.tinyint":   int8(127),
 	}
 
 	if !reflect.DeepEqual(m, expected) {
@@ -978,21 +978,21 @@ func TestTypesError(t *testing.T) {
 
 	d := cursor.Description()
 	expected := [][]string{
-		[]string{"all_types.boolean", "BOOLEAN_TYPE"},
-		[]string{"all_types.tinyint", "TINYINT_TYPE"},
-		[]string{"all_types.smallint", "SMALLINT_TYPE"},
-		[]string{"all_types.int", "INT_TYPE"},
-		[]string{"all_types.bigint", "BIGINT_TYPE"},
-		[]string{"all_types.float", "FLOAT_TYPE"},
-		[]string{"all_types.double", "DOUBLE_TYPE"},
-		[]string{"all_types.string", "STRING_TYPE"},
-		[]string{"all_types.timestamp", "TIMESTAMP_TYPE"},
-		[]string{"all_types.binary", "BINARY_TYPE"},
-		[]string{"all_types.array", "ARRAY_TYPE"},
-		[]string{"all_types.map", "MAP_TYPE"},
-		[]string{"all_types.struct", "STRUCT_TYPE"},
-		[]string{"all_types.union", "UNION_TYPE"},
-		[]string{"all_types.decimal", "DECIMAL_TYPE"},
+		{"all_types.boolean", "BOOLEAN_TYPE"},
+		{"all_types.tinyint", "TINYINT_TYPE"},
+		{"all_types.smallint", "SMALLINT_TYPE"},
+		{"all_types.int", "INT_TYPE"},
+		{"all_types.bigint", "BIGINT_TYPE"},
+		{"all_types.float", "FLOAT_TYPE"},
+		{"all_types.double", "DOUBLE_TYPE"},
+		{"all_types.string", "STRING_TYPE"},
+		{"all_types.timestamp", "TIMESTAMP_TYPE"},
+		{"all_types.binary", "BINARY_TYPE"},
+		{"all_types.array", "ARRAY_TYPE"},
+		{"all_types.map", "MAP_TYPE"},
+		{"all_types.struct", "STRUCT_TYPE"},
+		{"all_types.union", "UNION_TYPE"},
+		{"all_types.decimal", "DECIMAL_TYPE"},
 	}
 	if !reflect.DeepEqual(d, expected) {
 		t.Fatalf("Expected map: %+v, got: %+v", expected, d)
